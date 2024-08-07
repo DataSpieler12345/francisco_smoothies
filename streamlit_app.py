3dc99426f28a4ebe434d9ecfa2eff92513430a52
BI - Data Analyst <45371372+DataSpieler12345@users.noreply.github.com>
2024-08-07 21:57:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import Python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Set up the Snowflake connection
 cnx = st.connection("snowflake")
@@ -45,8 +46,10 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie has been ordered, {name_on_order}', icon="✅")
     
-    # New section to display fruityvice nutrition information
-    import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    # st.text(fruityvice_response.json())
-    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+if ingredients_list:
+        ingredients_string = ''
+
+        for fruit_chosen in ingredients_list:
+                ingredients_string += fruit_chose + ' ' 
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+                fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
